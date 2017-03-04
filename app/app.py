767dca83c227bf62e3465233e2d72af16caf143f
Ron Fenolio <ronf@syapse.com>
2017-03-04 01:45:19 -0800

    set env variables

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import os
 import requests
 
 app = Flask(__name__)
@@ -7,7 +8,7 @@ stuff = {'theFirst': 1,
             'theSecond': 2}
 
 DARK_URL = 'https://api.darksky.net/forecast/%s/%s,%s?exclude=[daily,hourly,minutely,alerts,flags]'
-OPEN_URL = 'http://api.openweathermap.org/data/2.5/weather?lat=%s&long=%s&APPID=%s'
+OPEN_URL = 'http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&APPID=%s'
 LAT = '40.7128'
 LONG = '-74.0059'
 
@@ -15,9 +16,8 @@ LONG = '-74.0059'
 @app.route('/')
 def root():
     lat = request.args.get('lat', LAT)
-    long = request.args.get('lat', LONG)
+    long = request.args.get('long', LONG)
     service = request.args.get('service', 'dark')
-    print service
     if service == 'dark':
         return jsonify(get_dark_weather(lat, long))
     elif service == 'open':
@@ -28,9 +28,9 @@ def root():
 
 
 def get_dark_weather(lat, long):
-    r = requests.get(DARK_URL %(DARK_API_KEY, lat, long))
+    r = requests.get(DARK_URL %(os.environ.get('DARK_API_KEY'), lat, long))
     return r.json()
 
 def get_open_weather(lat, long):
-    r = requests.get(OPEN_URL %(lat, long, OPEN_API_KEY))
+    r = requests.get(OPEN_URL %(lat, long, os.environ.get('OPEN_API_KEY')))
     return r.json()
